8311e2ec4a72b5b76c9a1c0fa813310f885d2a2d
Zapletin Daniil <Zaplot1994@gmail.com>
2023-01-30 03:06:30 +0300

    Task done with all tests passed

--- src/main/java/com/epam/rd/autotasks/Direction.java
+++ src/main/java/com/epam/rd/autotasks/Direction.java
@@ -1,6 +1,5 @@
 package com.epam.rd.autotasks;
 
-import java.util.Optional;
 
 public enum Direction {
     N(0), NE(45), E(90), SE(135), S(180), SW(225), W(270), NW(315);
@@ -9,21 +8,48 @@ public enum Direction {
         this.degrees = degrees;
     }
 
-    private int degrees;
+    private final int degrees;
 
     public static Direction ofDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        degrees = degreesToLimit(degrees);
+        for (Direction dir : Direction.values()){
+            if (dir.degrees == degrees) { return dir; }
+        }
+        return null;
     }
 
     public static Direction closestToDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        degrees = degreesToLimit(degrees);
+        int temp = Math.abs(degrees - Direction.N.degrees);
+        Direction out = Direction.N;
+        for (Direction dir : Direction.values()){
+            if (Math.abs(degrees - dir.degrees) <  temp) {
+                out = dir;
+                temp = Math.abs(degrees - dir.degrees);
+            }
+        }
+        return out;
     }
 
     public Direction opposite() {
-        throw new UnsupportedOperationException();
+        return this.ordinal() < 4 ?
+                Direction.values()[this.ordinal() + 4] :
+                Direction.values()[this.ordinal() -4];
     }
 
     public int differenceDegreesTo(Direction direction) {
-        throw new UnsupportedOperationException();
+        return Math.abs(this.degrees - direction.degrees) < 180 ?
+                Math.abs(this.degrees - direction.degrees) :
+                360 - Math.abs(this.degrees - direction.degrees);
+    }
+
+    public static int degreesToLimit(int degrees){
+        while (degrees >= 360 ){
+            degrees -= 360;
+        }
+        while (degrees < 0 ){
+            degrees += 360;
+        }
+        return  degrees;
     }
 }
